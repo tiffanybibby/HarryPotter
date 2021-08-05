701d5b341683df2aa9c9240d19bee7459b011aa1
Tiffany Bibby <tiffanybibby@Tiffanys-MBP.fios-router.home>
2021-08-05 09:09:42 -0400

    pmvp updates

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
 const url = 'http://hp-api.herokuapp.com/api/characters';
 const studentUrl = 'http://hp-api.herokuapp.com/api/characters/students';
 const staffUrl = 'http://hp-api.herokuapp.com/api/characters/staff';
-const houseUrl = 'http://hp-api.herokuapp.com/api/characters/house/:house';
 
-const dataContainer = document.querySelector('#data-container')
-const characterList = document.querySelector('ul.character-list')
 
-// console.log(form)
+
+const characterList = document.querySelector('.character-container')
+const selectTag = document.querySelector('#btn4')
+
+console.log(selectTag)
+
+// Function for All Characters List
 function displayAllCharacters() {
   const getAllCharacters = async () => {
+    remove(characterList)
     try {
       const response = await axios.get(url)
-      console.log(response)
+      // console.log(response)
       const data = response.data
-      console.log(data)
+      // console.log(data)
       buildElements(data)
-      setOptionTags(data)
 
     } catch (error) {
       console.error(error)
@@ -25,33 +28,106 @@ function displayAllCharacters() {
 }
 displayAllCharacters()
 
+// // Function for All Students List
+function displayStudents() {
+  const getStudents = async () => {
+    remove(characterList)
+    try {
+      const response = await axios.get(studentUrl)
+      // console.log(response)
+      const data = response.data
+      // console.log(data)
+      buildElements(data)
 
-// Form option tags from request
+    } catch (error) {
+      console.error(error)
+    }
+  }
+  getStudents()
+}
 
-function setOptionTags(list) {
-  console.log(list)
-  list.forEach((data) => {
-    // console.log(data)
+
+// //Function for All Staff List
+function displayStaff() {
+  const getStaff = async () => {
+    remove(characterList)
+    try {
+      const response = await axios.get(staffUrl)
+      // console.log(response)
+      const data = response.data
+      // console.log(data)
+      buildElements(data)
+
+    } catch (error) {
+      console.error(error)
+    }
+  }
+  getStaff()
+}
+
+//Function for All Houses Filter List
+function displayHouse() {
+  const getHouse = async () => {
+    const houseUrl = `http://hp-api.herokuapp.com/api/characters/house/${houses}`
+    remove(characterList)
+    try {
+      const response = await axios.get(houseUrl)
+      console.log(response)
+      const data = response.data
+      console.log(data)
+      buildElements(data)
+      const houses = [...new Set(data.map(({ house }) => house))]
+      console.log(houses)
+      setOptionTags(houses)
+
+    } catch (error) {
+      console.error(error)
+    }
+  }
+  getHouse(ravenclaw)
+}
+
+
+// Create form option tags from request
+
+function setOptionTags(house) {
+  // console.log(houses)
+  house.forEach((houses) => {
+    // console.log(house)
     const optionTag = document.createElement('option')
-    optionTag.value = data
-    optionTag.textContent = data
-    selectTag.append(optionTag)
+    optionTag.value = houses
+    optionTag.textContent = houses
+    selectTag.appendChild(optionTag)
   })
   return list
 }
 
 
+// Get value of option tag from the form select tag
+
+function getSelectValue(e) {
+  e.preventDefault()
+  const houseValue = selectTag.value
+  console.log(houseValue)
+  getBreedImage(houseValue)
+  return houseValue
+}
+
+
 //Event Handler
 
-form.addEventListener('submit', getSelectValue)
+document.getElementById("btn1").onclick = displayAllCharacters;
+document.getElementById("btn2").onclick = displayStudents;
+document.getElementById("btn3").onclick = displayStaff;
+document.getElementById("btn4").onclick = getSelectValue;
 
 //Build Elements
 
 function buildElements(data) {
   for (let i = 0; i < data.length; i++) {
-    console.log(data[i])
-    // const characterList = data[i].name
-    // console.log(characterList)
+    // console.log(data[i])
+    const characters = data[i].name
+    // console.log(characters)
 
 
     const cardContainer = document.createElement("div");
@@ -87,5 +163,11 @@ function buildElements(data) {
     Actor Name: ${data[i].actor}
     `
 
+  }
+}
+
+function remove(element) {
+  while (element.lastChild) {
+    element.removeChild(element.lastChild)
   }
 }
